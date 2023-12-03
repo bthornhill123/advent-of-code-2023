8b29f7b5f5eaf21a3fdac3d447fb0da749590616
Ben Thornhill <25141193+bthornhill123@users.noreply.github.com>
2023-12-03 16:03:54 -0700

    Complete day 1, part 1

--- golang/advent/one.go
+++ golang/advent/one.go
@@ -1,10 +1,58 @@
 package advent
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 type One struct {
 }
 
-func (d One) Run() {
-	fmt.Println("Hello, day 1!")
+func (d One) PartOne() error {
+	file, err := os.Open("inputs/one.txt")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	sum := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		first := d.firstDigit(line)
+		last := d.firstDigit(reverseString(line))
+		number, _ := strconv.Atoi(first + last)
+		sum += number
+	}
+
+	fmt.Println("Sum: ", sum)
+
+	return nil
+}
+
+func (d One) firstDigit(s string) string {
+	for _, c := range s {
+		// Check if the character is a numeric digit (0-9)
+		if c >= '0' && c <= '9' {
+			return string(c)
+		}
+	}
+
+	panic("No digit found in string")
+}
+
+func reverseString(s string) string {
+	// Convert string to a slice of runes
+	r := []rune(s)
+
+	// Reverse the slice
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+
+	// Convert the slice back to a string
+	return string(r)
 }
--- golang/main.go
+++ golang/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/bthornhill123/advent-of-code-2023/advent"
 )
 
 func main() {
 	day := advent.One{}
-	day.Run()
+	err := day.PartOne()
+	if err != nil {
+		fmt.Println("An error occurred: ", err)
+	}
 }
