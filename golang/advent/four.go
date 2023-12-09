598854d699b42e971b2bf23109a80fc29f3d7034
Ben Thornhill <25141193+bthornhill123@users.noreply.github.com>
2023-12-09 12:21:41 -0700

    Complete day 4, part 2

--- golang/advent/four.go
+++ golang/advent/four.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -52,6 +53,30 @@ func (d Four) PartOne() error {
 	return nil
 }
 
+func (d Four) parseLine(line string) (cardNum int, candidates, winners []int) {
+	numberPattern := regexp.MustCompile(`\d+`)
+
+	card := strings.Split(line, ":")[0]
+	cardNum, _ = strconv.Atoi(numberPattern.FindAllString(card, -1)[0])
+
+	numbers := strings.Split(line, ":")[1]
+	winnersStr := strings.Split(numbers, "|")[0]
+	winnerStrings := numberPattern.FindAllString(winnersStr, -1)
+	for _, winner := range winnerStrings {
+		winnerInt, _ := strconv.Atoi(winner)
+		winners = append(winners, winnerInt)
+	}
+
+	candidatesStr := strings.Split(numbers, "|")[1]
+	candidateStrings := numberPattern.FindAllString(candidatesStr, -1)
+	for _, candidate := range candidateStrings {
+		candidateInt, _ := strconv.Atoi(candidate)
+		candidates = append(candidates, candidateInt)
+	}
+
+	return
+}
+
 func (d Four) PartTwo() error {
 	file, err := os.Open("inputs/four.txt")
 	if err != nil {
@@ -61,11 +86,41 @@ func (d Four) PartTwo() error {
 
 	scanner := bufio.NewScanner(file)
 
+	copies := make(map[int]int)
+
 	for scanner.Scan() {
-		_ = scanner.Text()
+		cardNum, candidates, winners := d.parseLine(scanner.Text())
+
+		// Add a copy for the card number
+		copies[cardNum]++
+
+		// Create a set containing the winning numbers
+		winnersSet := make(map[int]bool)
+		for _, winner := range winners {
+			winnersSet[winner] = true
+		}
+
+		// Count the number of matches
+		matches := 0
+		for _, candidate := range candidates {
+			if _, ok := winnersSet[candidate]; ok {
+				matches++
+			}
+		}
+
+		// Each match adds a copy for each successive card
+		nextCard := cardNum + 1
+		for j := 0; j < matches; j++ {
+			copies[nextCard] += copies[cardNum]
+			nextCard++
+		}
 	}
 
 	sum := 0
+	for _, copies := range copies {
+		sum += copies
+	}
+
 	println("Sum: ", sum)
 
 	return nil
