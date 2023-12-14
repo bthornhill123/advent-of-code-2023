c4b90e980520e1c62c89cc577d829a62701ef53a
Ben Thornhill <25141193+bthornhill123@users.noreply.github.com>
2023-12-14 14:58:29 -0700

    Complete day 5

--- golang/advent/five.go
+++ golang/advent/five.go
@@ -16,14 +16,14 @@ type MapEntry struct {
 	length           int
 }
 
-func NewMapEntry(line string) *MapEntry {
+func NewMapEntry(line string) MapEntry {
 	numberPattern := regexp.MustCompile(`\d+`)
 	numbers := numberPattern.FindAllString(line, -1)
 	sourceStart, _ := strconv.Atoi(numbers[1])
 	destinationStart, _ := strconv.Atoi(numbers[0])
 	length, _ := strconv.Atoi(numbers[2])
 
-	return &MapEntry{
+	return MapEntry{
 		sourceStart:      sourceStart,
 		destinationStart: destinationStart,
 		length:           length,
@@ -94,5 +94,68 @@ func (d Five) PartOne() error {
 }
 
 func (d Five) PartTwo() error {
+	file, err := os.Open("inputs/five.txt")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	numberPattern := regexp.MustCompile(`\d+`)
+
+	seedsLine := scanner.Text()
+	seeds := numberPattern.FindAllString(seedsLine, -1)
+
+	seedSet := make(map[int]bool)
+
+	// Put all the seeds in a set
+	for i := 0; i < len(seeds); {
+		seed, _ := strconv.Atoi(seeds[i])
+		i++
+		seedRange, _ := strconv.Atoi(seeds[i])
+		i++
+		for j := seed; j <= seed+seedRange; j++ {
+			seedSet[j] = true
+		}
+	}
+
+	levels := make([][]MapEntry, 7)
+	mapLevel := -1
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// Skip empty lines
+		if len(line) == 0 {
+			continue
+		}
+
+		if line[0] >= '0' && line[0] <= '9' {
+			levels[mapLevel] = append(levels[mapLevel], NewMapEntry(line))
+		} else {
+			mapLevel++
+		}
+	}
+
+	lowestLocation := math.MaxInt
+	for value := range seedSet {
+		for i := 0; i < len(levels); i++ {
+			for _, level := range levels[i] {
+				nextValue, ok := level.getMapped(value)
+				if ok {
+					value = nextValue
+					break
+				}
+
+			}
+		}
+
+		if value < lowestLocation {
+			lowestLocation = value
+		}
+	}
+
+	println("Lowest Location: ", lowestLocation)
+
 	return nil
 }
